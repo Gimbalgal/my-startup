ee4cf7a956cc1c9b75d738c2e8f3423dc9b9b80b
Victoria Ishabo <victoriaishabo55@gmail.com>
2025-02-06 13:55:23 +0100

    makes some adjustment

--- src/Home/Team.js
+++ src/Home/Team.js
@@ -6,18 +6,20 @@ import developer from '../asset/developer.png';
 const Team = () => {
     return ( 
         <section className="workforce">
-            <div className="talent">
-                <h2>TEAM</h2>
-                <h1>Our Talents</h1>
-                <p>Lorem ipsum, dolor sit amet consectetur
-                Suscipit nemo hic quos, ab,</p>
-            </div>
-            <div className="our-talent">
-                <img src={ceo} alt="CEO" className='managers'/>
-                <img src={cto} alt="CTO" className='managers' />
-                <img src={designer} alt="DESIGNER" className='managers' />
-                <img src={developer} alt="DEVELOPER" className='managers'/>
-            </div>
+            <div className="workforce-container">
+                    <div className="talent">
+                        <h2>TEAM</h2>
+                        <h1>Our Talents</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur
+                        Suscipit nemo hic quos, ab,</p>
+                    </div>
+                    <div className="our-talent">
+                        <img src={ceo} alt="CEO" className='managers'/>
+                        <img src={cto} alt="CTO" className='managers' />
+                        <img src={designer} alt="DESIGNER" className='managers' />
+                        <img src={developer} alt="DEVELOPER" className='managers'/>
+                    </div>
+            </div>    
             <button className='view'>View Team</button>
         </section>
     );
